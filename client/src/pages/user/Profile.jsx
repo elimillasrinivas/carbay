0ffb2a551e1cd8137ab9455ee82998193474bd49
Srinivas Elimilla <114722049+elimillasrinivas@users.noreply.github.com>
2024-01-04 23:06:48 +0530

    Update Profile.jsx

--- client/src/pages/user/Profile.jsx
+++ client/src/pages/user/Profile.jsx
@@ -77,7 +77,7 @@ const Profile = ({ userData }) => {
   const handleUpdateProfile = async () => {
     try {
       const token = localStorage.getItem("token");
-      const response = await axios.put('http://localhost:3000/api/user/update',{
+      const response = await axios.put('https://server-hkuv.onrender.com/api/user/update',{
         userName:updatedName, email:updatedEmail, mobileNumber:updatedMobile
       },{
         headers:{
