f8b9af5e3ac10a35c2c6edf01e9cad72dfa3a866
Srinivas Elimilla <114722049+elimillasrinivas@users.noreply.github.com>
2024-01-15 06:51:34 +0530

    Update servicesRoutes.js

--- server/routes/servicesRoutes.js
+++ server/routes/servicesRoutes.js
@@ -3,9 +3,9 @@ const srvicesController = require('../controllers/srvicesController');
 
 const router = express.Router();
 
-// Signup route
 router.post('/create', srvicesController.createService);
 router.get('/get/:carModel', srvicesController.getServiceByCarModel);
+router.get('/get-images/:carCompany', srvicesController.getModelImagesByCarCompany);
 
 
 module.exports = router;
